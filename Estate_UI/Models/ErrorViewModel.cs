113279748d04ae48e5aa24fee16160b6b140207c
mehribanaskerofa <m.askerova.99@gmail.com>
2023-08-15 11:31:08 +0400

    Add project files.

---
+++ Estate_UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Estate_UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
